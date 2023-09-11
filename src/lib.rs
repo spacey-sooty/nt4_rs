5c6fbdcd29855bb04fe5aa215f10e3e0184b66e5
Isaac Turner <spacey_sooty@outlook.com>
2023-09-11 16:01:08 +0000

    started working on nt4

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,39 @@
+struct NTSubscriptionOptions {
+    periodic_rate_seconds: f32,
+    all: bool,
+    topics_only: bool,
+    prefix: bool,
+}
+
+impl NTSubscriptionOptions {}
+
+struct NTSubscription {
+    topic: String,
+    options: NTSubscriptionOptions,
+    uid: f32,
+}
+
+impl NTSubscription {
+    pub fn get_topic(&self) -> &String {
+        &self.topic
+    }
+
+    pub fn get_options(&self) -> &NTSubscriptionOptions {
+        &self.options
+    }
+
+    pub fn get_uid(&self) -> f32 {
+        self.uid
+    }
+
+    pub fn from(topic: String, options: NTSubscriptionOptions, uid: f32) -> Self {
+        NTSubscription {
+            topic,
+            options,
+            uid,
+        }
+    }
+}
+
+pub trait NT4 {}
 
