d21c4d7a34b614fff5c97b7de00fa720b63cf719
Isaac Turner <spacey_sooty@outlook.com>
2023-09-12 12:15:27 +0000

    split into client mod

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,50 @@
-struct NTSubscriptionOptions {
-    periodic_rate_seconds: f32,
-    all: bool,
-    topics_only: bool,
-    prefix: bool,
-}
-
-impl NTSubscriptionOptions {}
-
-struct NTSubscription {
-    topic: String,
-    options: NTSubscriptionOptions,
-    uid: f32,
-}
-
-impl NTSubscription {
-    pub fn get_topic(&self) -> &String {
-        &self.topic
+pub mod client {
+    struct NT4SubscriptionOptions {
+        periodic_rate_seconds: f32,
+        all: bool,
+        topics_only: bool,
+        prefix: bool,
     }
 
-    pub fn get_options(&self) -> &NTSubscriptionOptions {
-        &self.options
-    }
-
-    pub fn get_uid(&self) -> f32 {
-        self.uid
-    }
-
-    pub fn from(topic: String, options: NTSubscriptionOptions, uid: f32) -> Self {
-        NTSubscription {
-            topic,
-            options,
-            uid,
+    impl NT4SubscriptionOptions {
+        pub fn from(periodic_rate_seconds: f32, all: bool, topics_only: bool, prefix: bool) -> Self {
+            NT4SubscriptionOptions {
+                periodic_rate_seconds,
+                all,
+                topics_only,
+                prefix,
+            }
         }
     }
+
+    struct NT4Subscription {
+        topic: String,
+        options: NT4SubscriptionOptions,
+        uid: f32,
+    }
+
+    impl NT4Subscription {
+        pub fn get_topic(&self) -> &String {
+            &self.topic
+        }
+
+        pub fn get_options(&self) -> &NT4SubscriptionOptions {
+            &self.options
+        }
+
+        pub fn get_uid(&self) -> f32 {
+            self.uid
+        }
+
+        pub fn from(topic: String, options: NT4SubscriptionOptions, uid: f32) -> Self {
+            NT4Subscription {
+                topic,
+                options,
+                uid,
+            }
+        }
+    }
+
+    pub trait NT4Client {}
 }
 
-pub trait NT4 {}
